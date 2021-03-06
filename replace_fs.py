c8c4c33019cabc74341705d40cafd5ad0e6f74ed
fehlfarbe <marcus@degenkolbe.eu>
2021-03-07 00:38:04 +0100

    Added mklittlefs.exe for Windows

--- replace_fs.py
+++ replace_fs.py
@@ -1,3 +1,8 @@
 Import("env")
+from sys import platform
 print("Replace MKSPIFFSTOOL with mklittlefs")
-env.Replace (MKSPIFFSTOOL = "./update_fs.sh")
+# env.Replace (MKSPIFFSTOOL = "./update_fs.sh")
+if platform == "linux" or platform == "linux2":
+    env.Replace (MKSPIFFSTOOL = "./mklittlefs")
+elif platform == "win32":
+    env.Replace (MKSPIFFSTOOL = "./mklittlefs.exe")
